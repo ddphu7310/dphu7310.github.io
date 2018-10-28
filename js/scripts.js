7cd795c4c87083b2dc63979cc3e30652bbcb0cbe
ddphu7310 <44045087+ddphu7310@users.noreply.github.com>
2018-10-28 21:54:21 +1100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//NAVBAR TAB UNDERLINE ANIMATION//
+
+let menuIcon = document.querySelector('.menuIcon');
+let nav = document.querySelector('.overlay-menu');
+
+menuIcon.addEventListener('click', () => {
+    if (nav.style.transform != 'translateX(0%)') {
+        nav.style.transform = 'translateX(0%)';
+        nav.style.transition = 'transform 0.2s ease-out';
+    } else { 
+        nav.style.transform = 'translateX(-100%)';
+        nav.style.transition = 'transform 0.2s ease-out';
+    }
+});
